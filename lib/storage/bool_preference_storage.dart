21393482f39818d2a92fa6a7d099b8f0a6854193
darot-chen <chendarot1@gmail.com>
2021-05-02 21:25:50 +0700

    close #3 add share preference

---
+++ lib/storage/bool_preference_storage.dart
@@ -0,0 +1,26 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+abstract class BoolPreference {
+  String get key;
+
+  Future<void> toggle() async {
+    SharedPreferences _instance = await SharedPreferences.getInstance();
+    final bool current = await getBool() == true;
+    await _instance.setBool(key, !current);
+  }
+
+  Future<void> setBool({bool value}) async {
+    SharedPreferences _instance = await SharedPreferences.getInstance();
+    await _instance.setBool(key, value);
+  }
+
+  Future<void> clear() async {
+    SharedPreferences _instance = await SharedPreferences.getInstance();
+    await _instance.remove(key);
+  }
+
+  Future<bool> getBool() async {
+    SharedPreferences _instance = await SharedPreferences.getInstance();
+    return _instance.getBool(key);
+  }
+}
