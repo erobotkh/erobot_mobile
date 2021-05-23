63dcc2a5726f2a21e63653a9a73071b0b420b316
darot-chen <chendarot1@gmail.com>
2021-05-23 11:21:53 +0700

    close #24 ball shooter screen

---
+++ lib/screens/home/ball_shooter_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class BallShotterScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ball shotter '),
+      ),
+    );
+  }
+}
