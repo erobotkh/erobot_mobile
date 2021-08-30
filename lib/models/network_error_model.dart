be1fea7d7534b7bbe9ac44d610334f6c320e17a2
Darot <chendarot1@gmail.com>
2021-08-30 21:13:42 +0700

    close #69 set up api

---
+++ lib/models/network_error_model.dart
@@ -0,0 +1,7 @@
+class NetWorkErrorModel {
+  final String title;
+  final String description;
+  final int? statusCode;
+
+  NetWorkErrorModel({required this.title, required this.description, required this.statusCode});
+}
