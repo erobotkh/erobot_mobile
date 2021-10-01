43ee441a51a9ff026f36e5cee42c4e87af3ca6b7
Darot <chendarot1@gmail.com>
2021-10-01 23:29:52 +0700

    enhance login file structure and clean code

--- lib/screens/accounts/login_screen.dart
+++ lib/screens/accounts/login_screen.dart
@@ -1,75 +1,116 @@
 import 'package:erobot/config/config_constant.dart';
-import 'package:erobot/screens/accounts/local_widgets/e_dont_have_acc.dart';
-import 'package:erobot/screens/accounts/register_screen.dart';
+import 'package:erobot/constant/theme_constant.dart';
 import 'package:erobot/widgets/e_bottun.dart';
-import 'package:erobot/screens/accounts/local_widgets/e_textfield_text.dart';
-import 'package:erobot/screens/accounts/local_widgets/e_erobot.dart';
-import 'package:erobot/screens/accounts/local_widgets/e_forget_password.dart';
-import 'package:erobot/screens/accounts/local_widgets/e_textfield_password.dart';
 import 'package:flutter/material.dart';
 
 class AccountScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final _theme = Theme.of(context);
     return Scaffold(
-      backgroundColor: _theme.colorScheme.background,
-      appBar: _buildAppBar(_theme, context),
-      body: _buildBody(_theme, context),
-    );
-  }
-
-  _buildAppBar(ThemeData _theme, BuildContext context) {
-    return AppBar(
-      centerTitle: false,
-      backgroundColor: _theme.colorScheme.surface,
-      title: Text(
-        'Login',
-        style: _theme.textTheme.headline6,
+      appBar: AppBar(
+        centerTitle: false,
+        backgroundColor: Theme.of(context).colorScheme.surface,
+        title: Text(
+          'Login',
+          style: Theme.of(context).textTheme.headline6,
+        ),
+      ),
+      body: Column(
+        children: [
+          buildLogInHeader(context),
+          buildFormField(context),
+          Container(
+            padding: EdgeInsets.only(right: ConfigConstant.margin2),
+            alignment: Alignment.centerRight,
+            child: buildTextButton(context, 'Forget Password'),
+          ),
+          ButtonLogin(
+            name: 'LOGIN',
+            onTap: () {},
+          ),
+          buildTextButton(context, "Don't have account yet? Register"),
+        ],
       ),
     );
   }
 
-  _buildBody(ThemeData _theme, BuildContext context) {
-    return ListView(
-      children: [
-        Container(
-          child: Column(
-            children: [
-              Erobot(
-                  text: 'Member will have access privilege to see post, oportunity, and team reputation.',
-                  name: 'Erobot Member'),
-              Container(
-                padding: EdgeInsets.only(left: ConfigConstant.margin2, right: ConfigConstant.margin2),
-                child: Column(
-                  children: [
-                    Email(name: 'Email'),
-                    Password(name: 'Password'),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: ConfigConstant.margin1,
-              ),
-              ForgetPassword(
-                name: 'Forget Password?',
-                onTap: '',
-              ),
-              ButtonLogin(
-                name: 'LOGIN',
-                onTap: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => RegisterScreen(),
-                    ),
-                  );
-                },
-              ),
-              DontHaveAcc(name: 'Didn’t have account yet? Register', onTap: ''),
-            ],
+  InkWell buildTextButton(BuildContext context, String label) {
+    return InkWell(
+      onTap: () {},
+      child: Text(
+        label,
+        style: Theme.of(context).textTheme.caption,
+      ),
+    );
+  }
+
+  Container buildFormField(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(left: ConfigConstant.margin2, right: ConfigConstant.margin2),
+      margin: EdgeInsets.only(bottom: ConfigConstant.margin1),
+      child: Column(
+        children: [
+          TextField(
+            keyboardType: TextInputType.text,
+            decoration: InputDecoration(
+              hintStyle: Theme.of(context).textTheme.bodyText1,
+              labelText: 'Email',
+              labelStyle: Theme.of(context).textTheme.caption,
+            ),
           ),
-        )
-      ],
+          TextField(
+            obscureText: false,
+            autocorrect: true,
+            keyboardType: TextInputType.text,
+            decoration: InputDecoration(
+              hintStyle: Theme.of(context).textTheme.bodyText1,
+              labelText: 'Password',
+              labelStyle: Theme.of(context).textTheme.caption,
+              suffixIcon: IconButton(
+                icon: Icon(
+                  true ? Icons.visibility : Icons.visibility_off_rounded,
+                  color: Theme.of(context).colorScheme.onPrimary.withOpacity(0.5),
+                  size: 20,
+                ),
+                onPressed: () {},
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  AspectRatio buildLogInHeader(BuildContext context) {
+    ThemeData _theme = Theme.of(context);
+    return AspectRatio(
+      aspectRatio: 375 / 96,
+      child: Container(
+        padding: const EdgeInsets.only(right: 40, top: 10),
+        decoration: BoxDecoration(
+          gradient: ThemeConstant.redGradient,
+        ),
+        child: ListTile(
+          title: Text(
+            'Erobot Member',
+            style: _theme.textTheme.bodyText1,
+          ),
+          subtitle: Text(
+            'Member will have access privilege to see post, oportunity, and team reputation.',
+            style: _theme.textTheme.caption?.copyWith(
+              color: _theme.colorScheme.onPrimary.withOpacity(0.5),
+            ),
+          ),
+          trailing: IconButton(
+            icon: Icon(
+              Icons.people,
+              color: _theme.colorScheme.onPrimary,
+              size: 72,
+            ),
+            onPressed: () {},
+          ),
+        ),
+      ),
     );
   }
 }
