b169705fd38e9255437f59a083cc3549566b0b8a
theachoem <theacheng.g6@gmail.com>
2021-12-10 23:55:43 +0700

    migrate arduino car to thumbstick screen

--- lib/app/modules/thumbstick/views/thumbstick_view.dart
+++ lib/app/modules/thumbstick/views/thumbstick_view.dart
@@ -1,21 +1,79 @@
+import 'package:erobot_mobile/app/modules/joystick/widgets/widgets.dart';
+import 'package:erobot_mobile/constants/config_constant.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
-
 import '../controllers/thumbstick_controller.dart';
 
 class ThumbstickView extends GetView<ThumbstickController> {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight]);
     return Scaffold(
-      appBar: AppBar(
-        title: Text('ThumbstickView'),
-        centerTitle: true,
+      backgroundColor: Theme.of(context).colorScheme.background,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Theme.of(context).colorScheme.surface,
+        child: Icon(
+          Icons.settings,
+          color: Theme.of(context).colorScheme.onSurface,
+        ),
+        onPressed: () {
+          print('setting');
+        },
       ),
-      body: Center(
-        child: Text(
-          'ThumbstickView is working',
-          style: TextStyle(fontSize: 20),
+      appBar: CustomAppBar(
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+            SystemChrome.setPreferredOrientations([
+              DeviceOrientation.portraitUp,
+              DeviceOrientation.portraitDown,
+            ]);
+          },
+        ),
+        title: 'Thumbstick',
+        color: Colors.blue,
+        action: [
+          IconButton(
+            icon: Icon(
+              Icons.bluetooth,
+              color: Colors.white,
+            ),
+            onPressed: () => print('bluetooth'),
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(ConfigConstant.margin2),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    PadButtons(
+                      onLeft: () => print('onLeft'),
+                      onRight: () => print('onRight'),
+                      onUp: () => print('onUp'),
+                      onDown: () => print('onDown'),
+                    ),
+                    CircularSlider(
+                      color: Colors.blue,
+                      icon: Icons.flash_on,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
