12576f5cf894f0a4e0fda1c1481492e90fec1d72
Darot <64341213+darot-chen@users.noreply.github.com>
2022-04-02 15:16:00 +0700

    close #150 reauthenticate checking (#151)

--- lib/services/base_apis/retry_policy/refresh_token_policy.dart
+++ lib/services/base_apis/retry_policy/refresh_token_policy.dart
@@ -16,7 +16,7 @@ class RefreshTokenPolicy extends RetryPolicy {
     if (response.statusCode == 401) {
       if (response.body != null) {
         final json = jsonDecode(response.body!);
-        if (json['message'] == "JWT expired") {
+        if (json['message'] == "Invalid Token") {
           UserTokenModel? model = await auth.getCurrentUserToken();
           if (model?.refreshToken == null) return false;
           await auth.reAuthenticate(refreshToken: model!.refreshToken!);
